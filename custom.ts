af50905b1077a004f6eadb184e271afe650248e6
mkrhu-attila <48292141+mkrhu-attila@users.noreply.github.com>
2019-03-07 15:18:01 +0100

    Updates.

--- custom.ts
+++ custom.ts
@@ -2,13 +2,14 @@
  * Custom blocks
  */
 //% weight=100 color=#b6312f icon="" block="MakerspaceHu"
+//% groups=['Infra gate', 'others']
 namespace makerhu {
     /**
      * Converts milliseconds into seconds
      * @param value - Time in milliseconds eg:100
      * @param precision - Number of decimal places eg:2
      */
-    //% block="lapTimeInSeconds" precision.min=0 precision.max=3
+    //% block="lapTimeInSeconds" precision.min=0 precision.max=3 
     export function lapTimeSec(value: number, precision: number): number {
         return Math.roundWithPrecision(value / 1000, precision);
     }
@@ -17,7 +18,7 @@ namespace makerhu {
      * Flashes built-in LEDs when input is 1, stops when input is 0.
      * @param value  eg:0
      */
-    //% block="noise on screen %value"
+    //% block="noise on screen |%value"  
     export function noise(value: number): void {
         let dimension = 4
         if (value = 1) {
