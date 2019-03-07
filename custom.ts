3748dcaffa1028d625e98ea5c42eca280501f912
mkrhu-attila <48292141+mkrhu-attila@users.noreply.github.com>
2019-03-07 14:35:05 +0100

    bump

--- custom.ts
+++ custom.ts
@@ -17,14 +17,21 @@ namespace makerhu {
      * Flashes built-in LEDs when input is 1, stops when input is 0.
      * @param value  eg:0
      */
-    //% block advanced=true
-    export function noise(value: number): number {
-        return value <= 1 ? value : noise(value - 1) + noise(value - 2);
+    //% block="noise on screen %value" advanced=true
+    export function noise(value: number): void {
+        let dimension = 4
+        if (value = 1) {
+            led.toggle(Math.randomRange(0, dimension), Math.randomRange(0, dimension))
+        } else{}
     }
 
-    //% block="magnitude of 3d vector at x %x and y %y and z %z" advanced=true
+    /**
+     * Flashes built-in LEDs when input is 1, stops when input is 0.
+     */
+    //% block="noise on LED matrix %status" advanced=true
     //% inlineInputMode=inline
-    export function mag3d(x: number, y: number, z: number): number {
-        return Math.sqrt(x * x + y * y + z * z);
+    export function noisde(status: number ): number {
+        return Math.sqrt(status * status);
     }
+    
 }
